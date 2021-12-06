66e81bb8e2f5ec7c65a84927cabeb4f0a0d82282
Ju2ez <julianhatzky@googlemail.com>
2021-12-06 14:06:49 +0100

    Added d-separation + tests

--- BNReasoner.py
+++ BNReasoner.py
@@ -1,5 +1,6 @@
 from typing import Union
 from BayesNet import BayesNet
+import networkx as nx
 
 
 class BNReasoner:
@@ -16,16 +17,59 @@ class BNReasoner:
             self.bn = net
 
     # TODO: This is where your methods should go
+    def check_d_separation(self, X: str, Y: str, Z: list):
 
-    def d_seperate(self, X, Y, Z):
-        """
-        Check if X is independent of Y given Z by using d-separation.
-        """
-        # implemented the important properties of d-separation
+        net = self.bn
+        for path_list in nx.all_shortest_paths(net.structure.to_undirected(), X, Y):
+            path_length = len(path_list)
+            print(path_list, path_length)
+            if path_length < 3:
+                if path_length < 2:
+                    return True
+                elif path_length == 2:
+                    if path_list[0] in Z or path_list[1] in Z:
+                        return True
+                    else:
+                        return False
+
+            d_separated = False
+
+            for i in range(path_length - 2):
+                # check triplets
+                tr_1 = path_list[i]
+                tr_2 = path_list[i + 1]
+                tr_3 = path_list[i + 2]
+
+                if tr_2 in net.get_children(tr_1) and tr_3 in net.get_children(tr_2):
+                    # causal chain
+                    if tr_2 in Z:
+                        print('causal chain: d-separated')
+                        d_separated = True
+                        break
+
+                if tr_1 in net.get_children(tr_2) and tr_3 in net.get_children(tr_2):
+                    # common cause
+                    if tr_2 in Z:
+                        print('common cause: d-separated')
+                        d_separated = True
+                        break
+
+                if tr_2 in net.get_children(tr_1) and tr_2 in net.get_children(tr_3):
+                    # common effect
+
+                    if tr_2 not in Z and not \
+                            nx.descendants(net.structure, tr_2).intersection(Z):
+                        # check also descendents of middle Node
+                        print('common effect: d-separated')
+                        d_separated = True
+                        break
+
+            if d_separated is False:
+                print('Causal Path found')
+                return False
+
+        print('All possible paths are d-separated')
+        return True
+
+    def min_degree_heuristic(self):
         pass
-
-    def min_degree_heuristic(self,)
-
-
-
-
